f22a027dba53d49daed79a798a5285dd668717de
zerozero-0-0 <seapara.zerozero.00@gmail.com>
2026-02-22 20:56:09 +0900

    macに引き継ぎ

--- func/get_global_news.py
+++ func/get_global_news.py
@@ -1,14 +1,15 @@
-from aiohttp.web import get
 from dotenv import load_dotenv
 import os
 import requests
+from interfaces.data import JSONResponse, COREDATA
+from pydantic import ValidationError
 
 load_dotenv()
 
 NEWS_API_KEY = os.getenv("NEWS_API_KEY")
 
 if not NEWS_API_KEY:
-    raise ValueError("NEWS_API_KEY is not set in the environment variables.")
+    raise ValueError("NEWS_API_KEY is missing.")
 
 def get_global_news():
     url = "https://newsapi.org/v2/top-headlines"
@@ -20,6 +21,11 @@ def get_global_news():
         "apiKey": NEWS_API_KEY
     }
 
-    res = requests.get(url, params=params)
 
-    return res.json().get("articles", [])
+
+    res = requests.get(url, params=params)
+    payload = res.json()
+
+    
+if __name__ == "__main__":
+    get_global_news()
---
+++ interfaces/data.py
@@ -0,0 +1,29 @@
+from lib2to3.pytree import Base
+from pydantic import BaseModel
+
+
+class _SOURCE(BaseModel):
+    id: str | None
+    name: str
+
+
+class _data_class(BaseModel):
+    source: _SOURCE
+    author: str | None
+    title: str | None
+    description: str | None
+    url: str | None
+    urlToImage: str | None
+    publishedAt: str | None
+    content: str | None
+
+
+class JSONResponse(BaseModel):
+    status: str
+    totalResults: int
+    articles: list[_data_class]
+
+class COREDATA(BaseModel):
+    title: str
+    description: str
+    url: str
